c9580a6d17e8c9d3a913d126974e24795a167d57
hariom76 <hariommandal24@gmail.com>
2020-11-03 02:20:03 +0530

    deleted

--- myapp2/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-def index(request):
-    return HttpResponse('<body><h1>hows your girlfriend</h1><b>fuck off my girlfriend</b> </body>')
